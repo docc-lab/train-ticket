74b4e06c35ef47b9e302a432b7eaf67729fdb7c8
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-10-31 17:51:31 -0400

    Fix import

--- ts-cancel-service/src/main/java/cancel/controller/CancelController.java
+++ ts-cancel-service/src/main/java/cancel/controller/CancelController.java
@@ -16,7 +16,6 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import java.time.Instant;
 
 import static org.springframework.http.ResponseEntity.ok;
 

0f9401c3de2e893ce1ed68b70381a90eaaefb972
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-12-02 23:46:32 -0500

    debug

--- ts-cancel-service/src/main/java/cancel/service/AsyncConfig.java
+++ ts-cancel-service/src/main/java/cancel/service/AsyncConfig.java
@@ -13,10 +13,11 @@ public class AsyncConfig {
     @Bean
     public TaskDecorator traceContextDecorator() {
         return runnable -> {
-            // Create a new span at the same level as the original entry span
-            // Instead of creating it as a child span
             String parentTraceId = TraceContext.traceId();
-            return RunnableWrapper.of(runnable); 
+            return RunnableWrapper.of(() -> {
+                ActiveSpan.tag("parent.traceId", parentTraceId);
+                runnable.run();
+            });
         };
     }
 }

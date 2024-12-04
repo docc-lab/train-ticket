51748d0c35aaf8a93d31a06a580e6f5feb390bd4
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-12-04 05:08:34 -0500

    debug

--- ts-seat-service/src/main/java/seat/service/AsyncConfig.java
+++ ts-seat-service/src/main/java/seat/service/AsyncConfig.java
@@ -1,5 +1,7 @@
 package seat.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
--- ts-travel-service/src/main/java/travel/service/AsyncConfig.java
+++ ts-travel-service/src/main/java/travel/service/AsyncConfig.java
@@ -1,5 +1,7 @@
 package travel.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;

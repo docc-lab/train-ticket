6b0ddd1bb62fbf7535391ac0e719f96eff129c84
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-12-04 02:30:09 -0500

    Debug

--- ts-basic-service/src/main/java/fdse/microservice/service/AsyncConfig.java
+++ ts-basic-service/src/main/java/fdse/microservice/service/AsyncConfig.java
@@ -11,6 +11,7 @@ import org.apache.skywalking.apm.toolkit.trace.Tracer;
 import org.apache.skywalking.apm.toolkit.trace.ContextSnapshotRef;
 import org.apache.skywalking.apm.toolkit.trace.SpanRef;
 
+@Configuration
 public class AsyncConfig {
     @Bean
     public TaskDecorator traceContextDecorator() {

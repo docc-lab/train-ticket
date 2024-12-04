1d8462f9a56fa0898488448eb63681ea1befd28f
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-12-03 22:00:16 -0500

    debug

--- ts-travel2-service/src/main/java/travel2/service/Travel2ServiceConfiguration.java
+++ ts-travel2-service/src/main/java/travel2/service/Travel2ServiceConfiguration.java
@@ -4,25 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @Configuration
 public class Travel2ServiceConfiguration {
-    
+
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(3000);  // 3 seconds
         factory.setReadTimeout(5000);     // 5 seconds
         return new RestTemplate(factory);
-    }
-
-    @Bean
-    public IRule ribbonRule() {
-        return new RetryRule(new RoundRobinRule()); // Add retry with round-robin
-    }
-    
-    @Bean
-    public ILoadBalancer ribbonLoadBalancer() {
-        return new ZoneAwareLoadBalancer<>(); // Zone aware load balancing
     }
 }

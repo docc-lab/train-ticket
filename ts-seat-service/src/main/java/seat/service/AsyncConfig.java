9467af8583bd487d0fcf3b865c81c18c72fbc5cc
Zhaoqi(Roy) Zhang <zhaoqi.zhang@tufts.edu>
2024-12-04 02:35:14 -0500

    Debug

--- ts-seat-service/src/main/java/seat/service/AsyncConfig.java
+++ ts-seat-service/src/main/java/seat/service/AsyncConfig.java
@@ -10,6 +10,8 @@ import org.apache.skywalking.apm.toolkit.trace.ContextCarrierRef;
 import org.apache.skywalking.apm.toolkit.trace.Tracer;
 import org.apache.skywalking.apm.toolkit.trace.ContextSnapshotRef;
 import org.apache.skywalking.apm.toolkit.trace.SpanRef;
+
+@Configuration
 public class AsyncConfig {
     @Bean
     public TaskDecorator traceContextDecorator() {
